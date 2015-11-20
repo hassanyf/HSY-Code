b493aeac0261aa804b6bf897d2f655a2fb615497
hassanyf <mrxortax@gmail.com>
2015-11-20 23:20:14 +0500

    CSS Load

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task('jade', function() {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch('/assets/css/**', ['sass']);
      gulp.watch('assets/js/**', ['jekyll-rebuild']);
     gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
     gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
